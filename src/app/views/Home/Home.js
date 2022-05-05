6d4843b711a3f0d28c54c50d1d5f1ae9a67ef01d
Jair Zea <jairzeapaez@gmail.com>
2022-05-05 13:21:54 -0500

    add home component

--- src/app/views/Home/Home.js
+++ src/app/views/Home/Home.js
@@ -1,6 +1,13 @@
 import Cadillac from "../../assets/cadillac.jpg";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleSearchClick = () => {
+    navigate("/results/");
+  };
+
   return (
     <div className="flex h-screen overflow-hidden">
       <div className="w-2/5">
@@ -12,13 +19,22 @@ const Home = () => {
       </div>
 
       <div className="w-3/5 flex justify-center items-center flex-col px-10">
-        <h2 className="text-4xl font-bold font-lato">
-          ¿Quieres buscar un clasico?...
-        </h2>
-        <input className="bg-special-gray font-lato w-full my-3 h-9 p-1" />
-        <div>
-          <button>Buscar</button>
-          <button>Limpiar</button>
+        <h2 className="text-4xl font-bold font-lato">Clasicos de colección</h2>
+        {/* <input className="bg-special-gray font-lato w-full my-3 h-9 p-1 border focus:outline-none focus:ring-2 focus:ring-red-hover rounded" /> */}
+        <div className="flex w-full justify-between my-3">
+          <button
+            className="bg-blue text-white hover:bg-blue-hover font-lato w-full shadow-lg h-9 rounded"
+            style={{ width: "48%" }}
+          >
+            Añadir un clasico
+          </button>
+          <button
+            className="bg-red text-white hover:bg-red-hover font-lato w-full shadow-lg h-9 rounded"
+            style={{ width: "48%" }}
+            onClick={handleSearchClick}
+          >
+            Vehiculos Clasicos añadidos
+          </button>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
@@ -7,6 +9,12 @@ module.exports = {
     },
     colors: {
       "special-gray": "#F5F0F0",
+      ...defaultTheme.colors,
+      red: "#d36868",
+      blue: "#655ad3",
+      white: "#ffffff",
+      "red-hover": "#e13f3f",
+      "blue-hover": "#2f2789",
     },
     extend: {},
   },
